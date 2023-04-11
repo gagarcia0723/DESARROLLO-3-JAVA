1ed731a4135e6a156f2cb28c2c9f43d575878d84
gabriel <gagarcia@panama-tec.com>
2023-04-10 19:20:18 -0500

    creacion de nuevo entorno grafico

--- graficos_2/Grafico.java
+++ graficos_2/Grafico.java
@@ -1,7 +1,45 @@
-package graficos_2;
+import javax.swing.*;
 
 public class Grafico {
 
+    JFrame ventana;
+    JLabel lbl_nombre , lbl_apellido;
+    JTextField tf_nombre, tf_apellido;
     
+
+
+    public static void main(String[] args) {
+        new Grafico();
+
+    }
+    
+    Grafico(){
+
+        ventana = new JFrame("Mi ventana");
+        ventana.setSize(300,300);
+        ventana.setLocation(200,200);
+        ventana.setLayout(null);
+
+        lbl_nombre = new JLabel("Nombre:");
+        lbl_nombre.setBounds(50,50,70,20);
+        
+        tf_nombre = new JTextField();
+        tf_nombre.setBounds(125, 50, 100, 20);        
+
+        lbl_apellido = new JLabel("Apellido:");
+        lbl_apellido.setBounds(50,100,70,20);
+        
+        tf_apellido = new JTextField();
+        tf_apellido.setBounds(125, 100, 100, 20);
+        
+        ventana.add(lbl_nombre);
+        ventana.add(tf_nombre);
+        ventana.add(lbl_apellido);
+        ventana.add(tf_apellido);
+
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+
+    }
     
 }
