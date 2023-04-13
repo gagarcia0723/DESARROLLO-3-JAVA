ba477319f6bd5bd05ffcc04a43e2a431b8b850c5
gabrielgarcia <gagarcia@panama-tec.com>
2023-04-12 20:06:22 -0500

    Agregando un check box para restar

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -4,14 +4,15 @@ import java.awt.event.*;
 public class Calculadora implements ActionListener{
 
     /** 
-     *  Para iniciar con el desarrollo de ventanas es importante, 
+     *  Para iniciar con el desarrollo de VENTANAs es importante, 
      * definir el objeto de tipo JFrame con su respectivo nombre.
     */
 
-    JFrame ventana;
-    JLabel lblNum_1, lblNum_2, lblRes;
-    JTextField tfNum_1, tfNum_2, tfResultado;
+    JFrame VENTANA;
+    JLabel LBL_NUM1, LBL_NUM2, lBL_RES, LBL_NUM3;
+    JTextField TF_NUM1, TF_NUM2, TF_RESULTADO;
     JButton JB_CALCULAR;
+    JCheckBox JC_SUMA_RESTA;
 
 
     public static void main(String[] args) {
@@ -20,65 +21,88 @@ public class Calculadora implements ActionListener{
 
     Calculadora(){
 
-        ventana = new JFrame( "Mi ventana ");
-        ventana.setSize(500,300);
-        ventana.setLocation(300,300);
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.setLayout(null);
+        VENTANA = new JFrame( "Mi VENTANA ");
+        VENTANA.setSize(500,300);
+        VENTANA.setLocation(300,300);
+        VENTANA.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        VENTANA.setLayout(null);
 
-        lblNum_1 = new JLabel("NUMERO 1: ");
-        lblNum_1.setBounds(50,50, 100, 30);
-        tfNum_1 = new JTextField();
-        tfNum_1.setBounds(150,50,100,30);
+        LBL_NUM1 = new JLabel("NUMERO 1: ");
+        LBL_NUM1.setBounds(50,50, 100, 30);
+        TF_NUM1 = new JTextField();
+        TF_NUM1.setBounds(150,50,100,30);
 
-        lblNum_2 = new JLabel("NUMERO 2: ");
-        lblNum_2.setBounds(50,100, 100, 30);
-        tfNum_2 = new JTextField();
-        tfNum_2.setBounds(150,100,100,30);
+        LBL_NUM2 = new JLabel("NUMERO 2: ");
+        LBL_NUM2.setBounds(50,100, 100, 30);
+        TF_NUM2 = new JTextField();
+        TF_NUM2.setBounds(150,100,100,30);
         
-        lblRes = new JLabel("RESULTADO: ");
-        lblRes.setBounds(50,150, 100, 30);
-        tfResultado = new JTextField();
-        tfResultado.setBounds(150,150,100,30);
+        lBL_RES = new JLabel("RESULTADO: ");
+        lBL_RES.setBounds(50,150, 100, 30);
+        TF_RESULTADO = new JTextField();
+        TF_RESULTADO.setBounds(150,150,100,30);
 
         JB_CALCULAR = new JButton("Calcular");
         JB_CALCULAR.setBounds(300, 50, 100, 100);
+
         /**
-         * Esto es necesario agregar para poder hacer el reconocimiento de las pulsaciones realizadas
-         * 
+         * Esto es necesario agregar para poder hacer el reconocimiento
+         * de las pulsaciones realizadas.
          */
+
         JB_CALCULAR.addActionListener(this);
 
+        JC_SUMA_RESTA = new JCheckBox();
+        JC_SUMA_RESTA.setBounds(300, 150, 50,50);
+        
+        LBL_NUM3 = new JLabel("RESTAR ");
+        LBL_NUM3.setBounds(340,150, 300, 50);
+        
         /**
          *  ESTO ES LO NECESARIO PARA AGREGAR LOS COMPONENTES A LA VENTANA ANTES CREADA
          * 
-         * ventana.add(lblNum_1);
+         * VENTANA.add(LBL_NUM1);
          * 
          * */ 
 
-
-        ventana.add(lblNum_1);
-        ventana.add(lblNum_2);
-        ventana.add(lblRes);
-        ventana.add(tfNum_1);
-        ventana.add(tfNum_2);
-        ventana.add(tfResultado);
-        ventana.add(JB_CALCULAR);
+        VENTANA.add(LBL_NUM1);
+        VENTANA.add(LBL_NUM2);
+        VENTANA.add(LBL_NUM3);
+        VENTANA.add(lBL_RES);
+        VENTANA.add(TF_NUM1);
+        VENTANA.add(TF_NUM2);
+        VENTANA.add(TF_RESULTADO);
+        VENTANA.add(JB_CALCULAR);
+        VENTANA.add(JC_SUMA_RESTA);
         
-        ventana.setVisible(true);       
+
+        VENTANA.setVisible(true);       
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub\
+        // TODO Auto-generated method stub
 
         if( e.getSource() == JB_CALCULAR ) {
 
-            int RESULTADO = Integer.parseInt(tfNum_1.getText()) + Integer.parseInt(tfNum_2.getText());
-            tfResultado.setText( Integer.toString(RESULTADO ));
+            int RESULTADO = Integer.parseInt(TF_NUM1.getText()) + Integer.parseInt(TF_NUM2.getText());
+            TF_RESULTADO.setText( Integer.toString(RESULTADO ));
+
+            //JC_SUMA_RESTA.
 
         }
+
+        /* 
+        if( e.getSource() == JB_CALCULAR) {
+
+            int RESULTADO = Integer.parseInt(TF_NUM1.getText()) + Integer.parseInt(TF_NUM2.getText());
+            TF_RESULTADO.setText( Integer.toString(RESULTADO ));
+
+        }
+        */
+        
+
     }
 
 
